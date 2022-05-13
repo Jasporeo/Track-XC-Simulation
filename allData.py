8ec06d1d549283dece7a840ed18e317d1c80bd82
EshaanH <eshaan.hussain@gmail.com>
2022-05-12 20:22:22 -0700

    Bug fixing

---
+++ allData.py
@@ -0,0 +1,20 @@
+#This is the file you want to run to get all the CSV files. If you want to just work on some of the schools, just temporarily comment the rest out. Either way each school creates its own CSV file, so it's not really a big deal.
+
+import csv
+import ComprehensiveTopTeam
+
+ComprehensiveTopTeam.getTopSchool("1023", "CALIFORNIA-teamtimes")
+
+ComprehensiveTopTeam.getTopSchool("34018", "DOUGHERTY-teamtimes")
+
+ComprehensiveTopTeam.getTopSchool("1015", "DUBLIN-teamtimes")
+
+ComprehensiveTopTeam.getTopSchool("904", "BERKELEY-teamtimes")
+
+ComprehensiveTopTeam.getTopSchool("922", "CASTROVALLEY-teamtimes")
+
+ComprehensiveTopTeam.getTopSchool("927", "SANLEANDRO-teamtimes")
+
+ComprehensiveTopTeam.getTopSchool("933", "JAMESLOGAN-teamtimes")
+
+ComprehensiveTopTeam.getTopSchool("1008", "PITTSBURG-teamtimes")
